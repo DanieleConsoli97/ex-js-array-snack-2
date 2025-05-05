3c2024a7389b402348a57b52f21280ee22f4d4a2
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-05-05 15:20:07 +0200

    Snack2

--- Snack-2.js
+++ Snack-2.js
@@ -1,2 +1,20 @@
 import Books from "./data.js";
-console.log(Books)
+const availableBooks = Books.filter(book => book.available === true)
+console.log(availableBooks)
+const discountedBooks = availableBooks.map((book) => {
+    
+    const price = parseFloat(book.price.replace('€', ''))
+    const discountPrice = (Math.ceil(price * 20) / 100).toFixed(2)
+    return {
+        ...book,
+        price: `${discountPrice} €`
+    }
+}
+)
+console.log(discountedBooks)
+const  fullPricedBook = discountedBooks.find((book)=>{
+    const price = parseFloat(book.price.replace('€', ''))
+    return Number.isInteger(price) //  price % 1 === 0 
+})
+
+console.log(fullPricedBook)
