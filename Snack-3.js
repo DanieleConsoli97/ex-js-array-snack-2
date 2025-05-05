52cbea0bb4338169586c17fd1bad948666bf02d4
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-05-05 15:34:28 +0200

    snack3

--- Snack-3.js
+++ Snack-3.js
@@ -1,2 +1,13 @@
 import Books from "./data.js";
-console.log(Books)
+const authors = Books.map(book => book.author)
+console.log(authors)
+const areAuthorsAdults = authors.every(author => author.age >= 18)
+console.log(areAuthorsAdults)
+
+
+if (areAuthorsAdults === true) {
+    console.log(authors.sort((a, b) => a.age - b.age))
+} else {
+    console.log(authors.sort((a, b) => b.age - a.age))
+}
+
