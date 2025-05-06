3c83bbdbec64bd6867a33f11a98db044e9a2da6d
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-05-06 17:32:23 +0200

    snack 5 bonus

--- Snack-5.js
+++ Snack-5.js
@@ -1,2 +1,26 @@
 import Books from "./data.js";
-console.log(Books)
+
+const BookIds = [2, 13, 7, 21, 19]
+
+async function jsonPromise(url) {
+    const promiseFetch = await fetch(url)
+    const obj = await promiseFetch.json()
+    return obj
+}
+async function getBooks(ids) {
+    const PromiseArray = ids.map((id) => {
+        const promise = jsonPromise(`http://localhost:5000/books/${id}`)
+        return promise
+    })
+
+    console.log(PromiseArray)
+    const promiseJson = await Promise.all(PromiseArray)
+    return promiseJson
+
+}
+
+
+(async () => {
+    const promise = await getBooks(BookIds)
+    console.log(promise)
+})()
